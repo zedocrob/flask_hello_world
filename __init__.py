61bcbda0c3162d54a6297e33be8925b6e7be309f
bstocker <boris.stocker@gmail.com>
2024-01-21 16:04:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, jsonify
-import json
-import requests
-import sqlite3
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World1!"
-
+from flask import Flask
+from flask import render_template
+                                                                                                                                       
+app = Flask(__name__)                                                                                                                  
+                                                                                                                                       
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+                                                                                                                                       
 if __name__ == "__main__":
   app.run(debug=True)
