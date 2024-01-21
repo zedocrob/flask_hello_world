c3ed4e6e952a8b9483a1b06fb24b90908fd1d58b
bstocker <boris.stocker@gmail.com>
2024-01-21 16:41:51 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import render_template
 from flask import json
-import requests
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
