c231b379db3ecb090617b5154167e85dcb473534
zedocrob <zedocrob@gmail.com>
2024-02-02 15:07:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,6 +10,18 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
+@app.route('/paris/')
+def meteo():
+    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+
 @app.route("/fr/")
 def monfr():
     return "<h2>Bonjour tout le monde !</h2>"
