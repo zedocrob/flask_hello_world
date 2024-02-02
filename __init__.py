8359869bb3e5dc3b397ec1a3e00722707b7024af
zedocrob <zedocrob@gmail.com>
2024-02-02 14:27:35 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
+
+@app.route("/fr/")
+def monfr():
+    return "<h2>Bonjour tout le monde !</h2>"
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
